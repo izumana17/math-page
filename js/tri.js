5862b3bdd50bc6efb447f08b2b25080b2cacdd19
izumana17 <izumana17@gmail.com>
2023-02-18 15:05:36 +0600

    6th-triangle function updated

--- js/tri.js
+++ js/tri.js
@@ -3,15 +3,31 @@
 function tri(ans){
     const add=document.getElementById('divSec');
     const div=document.createElement('div');
+
     const p=document.createElement('span');
+    p.innerText='      Triangle  ';
 
     const pt=document.createElement('span');
-    pt.innerText='  Convert to m2';
+    pt.innerText=' Convert to m2 ';
 
-    p.innerText='      Triangle  '+ans;
+    
+    const pa=document.createElement('span');
+    pa.innerText=ans+' cm2';
+    pa.style.margin="0px 20px 0px 50px"
+
+
+
+
+    pt.style.backgroundColor='#1184e8';
+    pt.style.color='white';
+    pt.style.padding="8px";
+    pt.style.marginLeft="100px";
+    pt.style.border="2px solid white";
+    pt.style.borderRadius="5px";
     
 
     div.appendChild(p);
+    div.appendChild(pa);
     div.appendChild(pt);
     divSec.appendChild(div);
     
